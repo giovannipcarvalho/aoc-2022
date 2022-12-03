8dbcc29310a41fa49487c8781c2d80b572c1821a
Giovanni Paolo <giovanni1995@gmail.com>
2022-12-03 02:33:27 -0300

    day03: Add docstrings and rename variables for consistency

--- day03/main.py
+++ day03/main.py
@@ -4,6 +4,10 @@ priority = dict(zip(string.ascii_letters, range(1, 53)))
 
 
 def common_item_rucksack(sack: str) -> str:
+    """
+    Find the common item between the two compartments of a rucksack.
+    Each compartment is half of the sack.
+    """
     half = len(sack) // 2
     compartment1 = sack[:half]
     compartment2 = sack[half:]
@@ -11,6 +15,7 @@ def common_item_rucksack(sack: str) -> str:
 
 
 def sum_priorities_for_common_items(s: str) -> int:
+    """Sums the priorities the shared item in each rucksack"""
     total = 0
     for sack in s.splitlines():
         total += priority[common_item_rucksack(sack)]
@@ -18,6 +23,10 @@ def sum_priorities_for_common_items(s: str) -> int:
 
 
 def badge(group):
+    """
+    Find the badge for a group of sacks.
+    The common item among all sacks is the badge.
+    """
     x = set(group[0])
     for sack in group:
         x = x & set(sack)
@@ -25,6 +34,7 @@ def badge(group):
 
 
 def sum_priorities_for_group_badges(s: str) -> int:
+    """Sums the priorities for the identification badge of each group of sacks."""
     sacks = s.splitlines()
     n = 3
     groups = [sacks[i : i + n] for i in range(0, len(sacks), n)]
@@ -32,7 +42,7 @@ def sum_priorities_for_group_badges(s: str) -> int:
     return sum(priority[b] for b in badges)
 
 
-sample_case = """\
+sample_case_part1 = """\
 vJrwpWtwJgWrhcsFMMfFFhFp
 jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
 PmmdzqPrVvPwwTWBwg
@@ -51,8 +61,8 @@ CrZsJsPPZsGzwwsLwLmpwMDw
 """
 
 
-def test_sample_case():
-    assert sum_priorities_for_common_items(sample_case) == 157
+def test_sample_case_part1():
+    assert sum_priorities_for_common_items(sample_case_part1) == 157
 
 
 def test_sammple_case_part2():
