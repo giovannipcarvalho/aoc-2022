1a50d6bce3660d119b089ce14faec4dd12c4209c
Giovanni Paolo <giovanni1995@gmail.com>
2022-12-05 03:00:17 -0300

    day05: Refactor code and load crates automatically from input file

--- day05/main.py
+++ day05/main.py
@@ -1,39 +1,77 @@
-from copy import deepcopy
+import re
+from collections import defaultdict
 
-# remove the crates from input.txt XD
-CRATES = {
-    1: list("CFBLDPZS"),
-    2: list("BWHPGVN"),
-    3: list("GJBWF"),
-    4: list("SCWLFNJG"),
-    5: list("HSMPTLJW"),
-    6: list("SFGWCB"),
-    7: list("WBQMPTH"),
-    8: list("TWSF"),
-    9: list("RCN"),
-}
+
+def parse_crates(s: str) -> dict:
+    """
+    Parses crates and returns a dict where keys are the crate ID, and the values
+    are a list of items contained in the crate (top-most to bottom-most).
+    """
+    crates = defaultdict(list)
+
+    # ignore the last line with crate ids
+    for line in s.splitlines()[:-1]:
+        # append an empty char to make the line evenly divisible by 4
+        # split into chunks of 4
+        values = re.findall("....", line + " ")
+        for idx, v in enumerate(values, start=1):
+            if v[1] != " ":
+                crates[idx].append(v[1])
+    return crates
+
+
+def parse_command(line: str) -> tuple[int, int, int]:
+    """
+    Parses move commands
+
+    e.g.
+        >>> 'move 2 from 5 to 9'
+        (2, 5, 9)
+    """
+    _, amount, _, origin, _, dest = line.split(" ")
+    return int(amount), int(origin), int(dest)
+
+
+def parse_input(s: str) -> tuple[dict, list[tuple[int, int, int]]]:
+    """Parses input and returns crates and list of rearrengement instructions"""
+    _crates, _commands = s.split("\n\n")
+    crates = parse_crates(_crates)
+    commands = [parse_command(c) for c in _commands.splitlines()]
+    return crates, commands
 
 
 def solve1(s: str) -> str:
-    crates = deepcopy(CRATES)
-    for line in s.splitlines():
-        _, amount, _, origin, _, dest = line.split(" ")
-        for _ in range(int(amount)):
-            x = crates[int(origin)].pop(0)
+    """Follow moving instructions item-by-item"""
+    crates, commands = parse_input(s)
+    for amount, origin, dest in commands:
+        for _ in range(amount):
+            item = crates[origin].pop(0)
             # move crates to the top of the stack
-            crates[int(dest)].insert(0, x)
+            crates[dest].insert(0, item)
+
     return "".join([v[0] for k, v in crates.items()])
 
 
 def solve2(s: str) -> str:
-    crates = deepcopy(CRATES)
-    for line in s.splitlines():
-        _, amount, _, origin, _, dest = line.split(" ")
-        for i in range(int(amount)):
-            x = crates[int(origin)].pop(0)
-            # move crates in chunks i.e. insert into the original order
-            crates[int(dest)].insert(i, x)
+    """Follow moving instructions by moving chunks of items (maintaining order)"""
+    crates, commands = parse_input(s)
+    for amount, origin, dest in commands:
+        for i in range(amount):
+            item = crates[origin].pop(0)
+            # move crates in chunks i.e. insert in the original order
+            crates[dest].insert(i, item)
+
     return "".join([v[0] for k, v in crates.items()])
+
+
+def test_part1():
+    s = open("input.txt").read().strip()
+    assert solve1(s) == "FHSWJPSWM"
+
+
+def test_part2():
+    s = open("input.txt").read().strip()
+    assert solve2(s) == "PWHWFGPZS"
 
 
 if __name__ == "__main__":
