e5eac3d96ff53fb87359010c058294e297281345
Giovanni Paolo <giovanni1995@gmail.com>
2022-12-06 17:46:18 -0300

    day06: Add solution for part 2 and refactor

--- day06/main.py
+++ day06/main.py
@@ -1,12 +1,20 @@
 import pytest
 
 
-def solve1(s: str) -> int:
-    """Find the first index at which the previous 4 characters are unique."""
-    for i in range(4, len(s)):
-        if len(set(s[i - 4 : i])) == 4:
+def find_marker(s: str, n: int) -> int:
+    """Find the first index at which the previous `n` characters are unique."""
+    for i in range(n, len(s)):
+        if len(set(s[i - n : i])) == n:
             return i
     return -1
+
+
+def solve1(s: str):
+    return find_marker(s, 4)
+
+
+def solve2(s: str):
+    return find_marker(s, 14)
 
 
 @pytest.mark.parametrize(
@@ -19,10 +27,25 @@ def solve1(s: str) -> int:
         ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11),
     ],
 )
-def test_sample(input, expected):
+def test_sample_part1(input, expected):
     assert solve1(input) == expected
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19),
+        ("bvwbjplbgvbhsrlpgdmjqwftvncz", 23),
+        ("nppdvjthqldpwncqszvftbrmjlhg", 23),
+        ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29),
+        ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26),
+    ],
+)
+def test_sample_part2(input, expected):
+    assert solve2(input) == expected
 
 
 if __name__ == "__main__":
     s = open("input.txt").read()
     print("part 1:", solve1(s))
+    print("part 2:", solve2(s))
